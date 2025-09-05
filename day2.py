7a8e5b70719ac4fa4a9b329435c68ac7c178acf8
mehedi imun <mehediimun@gmail.com>
2025-09-05 19:49:20 +0600

    Add initial implementation of string manipulation and operator examples

---
+++ day2.py
@@ -0,0 +1,62 @@
+# a = 10
+# b = 3
+# Arithmetic Operators
+# print("Addition:", a + b)
+# print("Subtraction:", a - b)
+# print("Multiplication:", a * b)
+# print("Division:", a / b)   # float
+# print("Floor Division:", a // b)  # integer division
+# print("Modulus:", a % b)    # remainder
+# print("Exponent:", a ** b)  # a^b
+
+# Comparison Operators
+# print(a > b)   # False
+# print(a < b)   # True
+# print(a == b)  # False
+# print(a != b)  # True
+# print(a >= b)  # False
+# print(a <= b)  # True
+
+
+# Logical Operators
+# x = True
+# y = False
+# print(x and y)
+# print(x or y)
+
+
+
+
+# Strings Basics
+# name = 'mehedi'
+# print(name[0])
+# print(name[-1])
+# print(name[0:3])
+# print(name + " Imun")
+# print(name * 2 ) 
+
+
+# Useful String Methods
+# s = "hOllo world"
+# print(s.upper()) 
+# print(s.lower()) 
+# print(s.title())  
+# print(s.strip())  
+# print(s.split()) 
+# print(" ".join(['hellso','world'])) 
+# print(s.replace("world","python"))
+
+s = input("string:")
+# print(str[::-1])
+# print(len(str.split()))
+# if str ==str[::1]:
+#     print("Palindrome")
+# else:
+#     print("not Palindrome")
+first = s[0]
+last = s[-1]
+middle = s[len(s)//2]
+
+print("First:", first)
+print("Last:", last)
+print("Middle:", middle)
