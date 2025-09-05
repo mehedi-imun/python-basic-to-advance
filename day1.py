60caabe2f5efa46634da5b2142a1a15811696b87
mehedi imun <mehediimun@gmail.com>
2025-09-05 19:19:17 +0600

    first commit

---
+++ day1.py
@@ -0,0 +1,31 @@
+print("Hello, DSA with Python!")
+num = 10
+
+name = "John Doe"
+# name = 10
+print(name+str(num))
+# print(name)
+bangladesh = "bangladesh"
+popuation = 170_000_000
+
+print(bangladesh,"has",str(popuation))
+
+#  input output 
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(name , int(age)+2)
+
+age = input("Enter your age: ")
+num = int(input("ender number:"))
+nu2 = int(input("ender number:"))
+
+
+
+
+num = int(input("ender number:"))
+
+
+if num % 2 ==0:
+    print(num,"num is even")
+else:
+    print(num,"is ODD")
